29334a6c4e81d4d6e5fb8c3d9fe80216ed78d4e2
HasanMukit <mukit.hasan07@gmail.com>
2021-04-05 21:31:36 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
-const apiUrl = "https://script.google.com/macros/s/AKfycbzc0eg0WHXP4_Tcx435XPz2ECT3MaMPZE8jmhx70-6PtvUDoq5ylt2eUVstGb9zmo2-/exec";
+let resultArry;
+const apiUrl = "https://script.google.com/macros/s/AKfycbwchVOxBVGBQslCB9dtkLhKyj4pyihAxsWCTUBqJgZqd1ipJ8zqrVLFncAe8nBGxZId/exec";
 
 
 
@@ -7,9 +7,9 @@ function searchFunc() {
   let searchInput = document.getElementById("searchField").value;
   
   if(searchInput.length > 4 ) {
+
     const isEmailValid = emailValidation(searchInput);
     
-
     if(isEmailValid)
     {
       getData(searchInput)
@@ -17,59 +17,11 @@ function searchFunc() {
     else
     {
       alert("Invalid Email")
-    }
-    
-  
+    }  
   }
 
+
   
-  // let resultArry = data.filter(function(r){
-  //   return r[3].toString().toLowerCase() === searchInput.toString().toLowerCase()
-  // })
-
-  // let searchResultBox = document.getElementById("searchResults");
-  // let templatetBox = document.getElementById("rowTemplate");
-  // let template = templatetBox.content;
-
-  // searchResultBox.innerHTML = "";
-
-
-  // resultArry.forEach(function(r,i){
-
-  // let tr = template.cloneNode(true)
-
-  // let idCol = tr.querySelector(".sn")
-  // let storNameCol = tr.querySelector(".storeName")
-  // let fnCol = tr.querySelector(".firstName")
-  // let lnCol = tr.querySelector(".lastName")
-  // let emailCol = tr.querySelector(".email")
-  // let feinCol = tr.querySelector(".fien")
-  // let phnCol = tr.querySelector(".phn")
-  // let addCol = tr.querySelector(".addrs")
-  // let subCol = tr.querySelector(".sub")
-  // let blcCol = tr.querySelector(".blc")
-  // let tlcCol = tr.querySelector(".tlc")
-  // let localeCol = tr.querySelector(".locale")
-  // let subSrcCol = tr.querySelector(".subSrc")
-
-  // idCol.textContent = i+1;
-  // storNameCol.textContent = r[0]
-  // fnCol.textContent = r[1]
-  // lnCol.textContent = r[2]
-  // emailCol.textContent = r[3]
-  // feinCol.textContent = r[4]
-
-  // phnCol.textContent = r[5]
-  // console.log(i)
-  // addCol.textContent = r[6]
-  // subCol.textContent = r[7]
-
-  // blcCol.href = r[8]
-  // tlcCol.href = r[9]
-  // localeCol.textContent = r[10]
-  // subSrcCol.href = r[11]
-  // searchResultBox.appendChild(tr);
-  // });
 }
 
 time()
@@ -83,33 +35,29 @@ setTimeout(function() {
 }
 
 // doPost test
-async function getData(input) {
-  
-//   const response = await fetch(apiUrl, {
-//         method: 'POST', 
-//         mode: 'no-cors',
-//         cache: 'no-cache',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         redirect: 'follow', 
-//         body: JSON.stringify({email:input}) 
-//       })
-//         .then(d => {console.log(d)})
-  axios({
-    method: 'post',
-    url: apiUrl,
-    mode: 'cors',
-    data: {
-      email: input
-    }
-  })
-    .then(res => console.log(res))
-    
-  
-  
-}
 
+  
+
+function getData(email) {
+  fetch(apiUrl)
+      .then(d => d.json())
+      .then(d => {
+          const nonFilterJson = Object.values(d[0].data)          
+          resultArry = nonFilterJson.filter(function(r){
+          return r.email.toString().toLowerCase() === email.toString().toLowerCase()})
+          console.log(resultArry)
+          populateTable(resultArry)
+          
+      });
+  }
+// let header = document.createElement("p");
+// fetch(apiUrl)
+//     .then(d => d.json())
+//     .then(d => {
+//         header.innerHTML = d[0].status
+//         document.getElementById('cont').prepend(header)
+          
+//     });
 
 
 document.querySelector('.btn').addEventListener('click', searchFunc)
@@ -119,4 +67,50 @@ document.querySelector('.btn').addEventListener('click', searchFunc)
 function emailValidation(email) {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
+}
+
+function populateTable(resultArry) {
+  let searchResultBox = document.getElementById("searchResults");
+  let templatetBox = document.getElementById("rowTemplate");
+  let template = templatetBox.content;
+
+  searchResultBox.innerHTML = "";
+
+
+  resultArry.forEach(function(r,i){
+
+  let tr = template.cloneNode(true)
+
+  let idCol = tr.querySelector(".sn")
+  let storNameCol = tr.querySelector(".storeName")
+  let fnCol = tr.querySelector(".firstName")
+  let lnCol = tr.querySelector(".lastName")
+  let emailCol = tr.querySelector(".email")
+  let feinCol = tr.querySelector(".fien")
+  let phnCol = tr.querySelector(".phn")
+  let addCol = tr.querySelector(".addrs")
+  let subCol = tr.querySelector(".sub")
+  let blcCol = tr.querySelector(".blc")
+  let tlcCol = tr.querySelector(".tlc")
+  let localeCol = tr.querySelector(".locale")
+  let subSrcCol = tr.querySelector(".subSrc")
+
+  idCol.textContent = i+1;
+  storNameCol.textContent = r.store
+  fnCol.textContent = r.firstName
+  lnCol.textContent = r.lastName
+  emailCol.textContent = r.email
+  feinCol.textContent = r.fien
+
+  phnCol.textContent = r.phn 
+  
+  addCol.textContent = r.addrs
+  subCol.textContent = r.sub
+
+  blcCol.href = r.blc
+  tlcCol.href = r.tlc
+  localeCol.textContent = r.locale
+  subSrcCol.href = r.subSrc
+  searchResultBox.appendChild(tr);
+   });
 }
