5983c8e61d85d8aaefa7ba9b308a6106c63517fc
Hasan Mukit <mukit.hasan07@gmail.com>
2022-02-23 16:38:23 +0600

    added new database

--- public/index.js
+++ public/index.js
@@ -1,48 +1,39 @@
-
-document.querySelector('.btn').addEventListener('click', searchFunc)
+document.querySelector(".btn").addEventListener("click", searchFunc);
 
 function searchFunc() {
-  document.getElementById('search').style.display = "flex"
-  let searchBox = document.getElementById("searchField")
-  let searchInput = searchBox.value
+  document.getElementById("search").style.display = "flex";
+  let searchBox = document.getElementById("searchField");
+  let searchInput = searchBox.value;
   document.getElementsByClassName("table-container")[0].style.display = "none";
-  if(!document.body.classList.contains("loading-body"))
-  {
-    document.body.classList.add("loading-body")
+  if (!document.body.classList.contains("loading-body")) {
+    document.body.classList.add("loading-body");
   }
-  
-  
-  
-  if(isEmail(searchInput))
-  {
-    
-    document.getElementById('search').style.display = "none"
+
+  if (isEmail(searchInput)) {
+    document.getElementById("search").style.display = "none";
     document.getElementsByClassName("spinner-box")[0].style.display = "flex";
-    
-    fetch('/search?email=' + searchInput).then((response) => {
-      response.json().then((data) =>{
-        if(data.length === 0) {
-          alert("No data associated with the provided email")
+
+    fetch("/search?email=" + searchInput).then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+        if (data.length === 0) {
+          alert("No data associated with the provided email");
+        } else {
+          populateTable(data);
+          document.body.classList.remove("loading-body");
+          document.getElementsByClassName("table-container")[0].style.display =
+            "block";
         }
-        else
-        {
-          populateTable(data)
-          document.body.classList.remove("loading-body")
-          document.getElementsByClassName("table-container")[0].style.display = "block";
-          
-        }
-        document.getElementsByClassName("spinner-box")[0].style.display = "none";
-    
-        document.getElementById('search').style.display = "flex"
-      })
-    })
-    
+        document.getElementsByClassName("spinner-box")[0].style.display =
+          "none";
+
+        document.getElementById("search").style.display = "flex";
+      });
+    });
+  } else {
+    alert("Invalid Email");
   }
-  else
-  {
-    alert("Invalid Email")
-  }
-  searchBox.value = ""
+  searchBox.value = "";
 }
 
 //hide loading screen
@@ -54,55 +45,45 @@ function searchFunc() {
 
 // }
 
-
 function isEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
 
 function populateTable(resultArry) {
   let searchResultBox = document.getElementById("searchResults");
-  searchResultBox.innerHTML = ''
+  searchResultBox.innerHTML = "";
   let templatetBox = document.getElementById("rowTemplate");
   let template = templatetBox.content;
+  console.log(resultArry);
+  resultArry.forEach(function (r, i) {
+    let tr = template.cloneNode(true);
 
+    let idCol = tr.querySelector(".sn");
+    let storeCol = tr.querySelector(".store");
+    let addressCol = tr.querySelector(".address");
+    let cityCol = tr.querySelector(".city");
+    let stateCol = tr.querySelector(".state");
+    let zipCol = tr.querySelector(".zip");
+    let licenseCol = tr.querySelector(".license");
+    let expires_atCol = tr.querySelector(".expires_at");
+    let tlcCol = tr.querySelector(".tlc");
 
+    idCol.textContent = i + 1;
+    storeCol.textContent = r.store;
+    addressCol.textContent = r.address;
+    cityCol.textContent = r.city;
+    stateCol.textContent = r.state;
+    zipCol.textContent = r.zip;
+    licenseCol.textContent = r.license;
+    expires_atCol.textContent = r.expires_at.split("T")[0];
+    if (r.tlc.trim().length > 0) {
+      tlcCol.href = r.tlc;
+    } else {
+      tlcCol.href = "javascript:alert('File Not Yet Available')";
+    }
 
-  resultArry.forEach(function(r,i){
-
-  let tr = template.cloneNode(true)
-
-  let idCol = tr.querySelector(".sn")
-  let storNameCol = tr.querySelector(".storeName")
-  let fnCol = tr.querySelector(".firstName")
-  let lnCol = tr.querySelector(".lastName")
-  let emailCol = tr.querySelector(".email")
-  let feinCol = tr.querySelector(".fien")
-  let phnCol = tr.querySelector(".phn")
-  let addCol = tr.querySelector(".addrs")
-  let blcCol = tr.querySelector(".blc")
-  let tlcCol = tr.querySelector(".tlc")
-  
-
-  idCol.textContent = i+1;
-  storNameCol.textContent = r.store
-  fnCol.textContent = r.firstName
-  lnCol.textContent = r.lastName
-  emailCol.textContent = r.email
-  feinCol.textContent = r.fien
-
-  phnCol.textContent = r.phn 
-  
-  addCol.textContent = r.addrs
-  
-
-  r.blc.trim().length > 0 ? blcCol.href = r.blc : blcCol.href ="javascript:alert('File Not Yet Available')";
-
-  r.tlc.trim().length > 0 ? tlcCol.href = r.tlc : tlcCol.href ="javascript:alert('File Not Yet Available')";
- 
-  
-  searchResultBox.appendChild(tr);
-   });
+    searchResultBox.appendChild(tr);
+  });
 }
-
-
--- src/data.js
+++ src/data.js
@@ -1,49 +1,45 @@
-const request = require('request')
-
-
+const request = require("request");
 
 const data = (email, callback) => {
-    const url = 'https://script.googleusercontent.com/a/macros/juicesupplycompany.com/echo?user_content_key=JRcfc79PQd6fSiswEMgb8h-1Uvlk1g-iIjhXyiT2OU59OeKwJrMrf7IuIogY5HVplca5RjIH-jdIBUbDOD0nurrzPlcLG2XiOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKCPN61pSWtBEo-3pL4KcNPpXa3lSn6LDfpoAPxoDu5zVzmjbbMW9ZEiTq4_eFHiiEYGJVVrrcLn94tlJBvksOqimzBIu5f_SQ2y2dH5QhfK7s76CDEpq_XZZPpA76_vnHbHqLXJsnpwzNz9Jw9Md8uu&lib=MWCVIWCdHMAb33AphfpbKETJdmpy7TQ7w'
-    if(!isEmail(email)){
-        const result = {
-            status : 401,
-            data : null
-        }
-        callback(undefined, result)
-    }
-    else
-    {
-        request({url}, (error, response) => {
-            if(error) {
-                    callback('Unable to connect', undefined)
-                }
-                else {
-                    const data = JSON.parse(response.body)[0].data
-                    const resultArr = filterData(email,data)
-                    const result = {
-                        status : 201,
-                        data : resultArr
-                    }
-                    callback(undefined, result)
-                }
-            })
-            
-    }
-}
+  const baseURL =
+    "https://script.google.com/macros/s/AKfycbxIeJgXFpWXxYSGRpq0WBI8IlK0TPQlPWCysFXnn-qOs1JbirVtGl1dILflJnQJKUKT/exec";
 
+  if (!isEmail(email)) {
+    const result = {
+      status: 401,
+      data: null,
+    };
+    callback(undefined, result);
+  } else {
+    let url = baseURL + "?email=" + email;
+    request({ url }, (error, response) => {
+      if (error) {
+        callback("Unable to connect", undefined);
+      } else {
+        const data = JSON.parse(response.body)[0].data;
+        const resultArr = filterData(email, data);
+        const result = {
+          status: 201,
+          data: resultArr,
+        };
+        callback(undefined, result);
+      }
+    });
+  }
+};
 
 function isEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  const re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
 
 function filterData(email, mainData) {
-    const resultArry = mainData.filter(function(r){
-        return r.email.toString().toLowerCase() === email
-    })
-      
-      return resultArry;
-  }
+  const resultArry = mainData.filter(function (r) {
+    return r.email.toString().toLowerCase() === email;
+  });
 
-module.exports = data
- 
+  return resultArry;
+}
+
+module.exports = data;
